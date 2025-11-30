18aa9aa6d494032b5df0628d82ee9418f0e12e6b
Zaid Khan <160540910+zaidk99@users.noreply.github.com>
2025-11-30 19:49:33 +0530

    fixed typo

--- backend/scripts/buildGlobalTrie.js
+++ backend/scripts/buildGlobalTrie.js
@@ -4,7 +4,6 @@ import { tokenize } from "../utils/tokenize.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// __dirname fix for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
